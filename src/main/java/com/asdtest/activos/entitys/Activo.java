ac136bcc0226ca006a24fb3758af83dc2ddfadf9
Pholluxion <penalozadaniel00@gmail.com>
2022-02-15 11:19:26 -0500

    commit

--- src/main/java/com/asdtest/activos/entitys/Activo.java
+++ src/main/java/com/asdtest/activos/entitys/Activo.java
@@ -16,6 +16,102 @@ private String broad;
 private String longt;
 private String purchaseValue;
 private String dateOfPurchase;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+
+    public String getInternalNumber() {
+        return internalNumber;
+    }
+
+    public void setInternalNumber(String internalNumber) {
+        this.internalNumber = internalNumber;
+    }
+
+    public String getStockNumber() {
+        return stockNumber;
+    }
+
+    public void setStockNumber(String stockNumber) {
+        this.stockNumber = stockNumber;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getHigh() {
+        return high;
+    }
+
+    public void setHigh(String high) {
+        this.high = high;
+    }
+
+    public String getBroad() {
+        return broad;
+    }
+
+    public void setBroad(String broad) {
+        this.broad = broad;
+    }
+
+    public String getLongt() {
+        return longt;
+    }
+
+    public void setLongt(String longt) {
+        this.longt = longt;
+    }
+
+    public String getPurchaseValue() {
+        return purchaseValue;
+    }
+
+    public void setPurchaseValue(String purchaseValue) {
+        this.purchaseValue = purchaseValue;
+    }
+
+    public String getDateOfPurchase() {
+        return dateOfPurchase;
+    }
+
+    public void setDateOfPurchase(String dateOfPurchase) {
+        this.dateOfPurchase = dateOfPurchase;
+    }
     
     
     
--- src/main/java/com/asdtest/activos/rest/ActivosREST.java
+++ src/main/java/com/asdtest/activos/rest/ActivosREST.java
@@ -1,6 +1,5 @@
 package com.asdtest.activos.rest;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class ActivosREST {
-
-//    @GetMapping
     @RequestMapping(value = "hello", method = RequestMethod.GET)
     public String hello() {
         return "<p>Hello world</p>";
